4c94e28c6b6775c84bb0070cb0025eccfb27ae0a
Lanliss <lanliss.rollenspel@gmail.com>
2024-12-24 18:33:28 +0100

    commented unnecassary print

--- Scripts/CameraScript.cs
+++ Scripts/CameraScript.cs
@@ -51,7 +51,7 @@ public partial class CameraScript : Camera3D
 		{
 			var mousePosition = (Vector3)rayCastResult["position"];
             EmitSignal(SignalName.MousePosFound, mousePosition);
-			GD.Print("Mouse Pos Found");
+			//GD.Print("Mouse Pos Found");
 		}
 		
 
