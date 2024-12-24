ec395e10336c75b49824f1e8dc9ebfa44c817abb
Lanliss <lanliss.rollenspel@gmail.com>
2024-12-24 18:41:44 +0100

    pre merge commit

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd19b951944453193790542cd112c3c1eac7bac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c94e28c6b6775c84bb0070cb0025eccfb27ae0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
