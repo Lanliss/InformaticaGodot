03a577f532a4342ecfa34f47eb9fde2f9e05cb26
ozzo609 <necromancer608@gmail.com>
2024-12-24 17:15:43 +0100

    test.oesman branch???

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd19b951944453193790542cd112c3c1eac7bac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5026ccaf445750c0ddc9753ba4ed5d73d1e2286e")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
