8074ef7ba65aee7fc47650ce229bc95d84bd1f1a
Lanliss <lanliss.rollenspel@gmail.com>
2024-12-27 16:16:36 +0100

    realm commit

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5026ccaf445750c0ddc9753ba4ed5d73d1e2286e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f98c5516fb05452619abc7aa328e55435b73d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
