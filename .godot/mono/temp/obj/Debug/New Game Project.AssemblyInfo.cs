42a6e6d344cbd456de81b263cfce53dc9a9aee77
Lanliss <lanliss.rollenspel@gmail.com>
2025-01-18 14:43:45 +0100

    level 1 updated not done tho

--- .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/New Game Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45cb2d52fa32fca6ded9ada5a62df7c8e114924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b465ad29000ed073103f32b99c5e25507bbecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New Game Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
